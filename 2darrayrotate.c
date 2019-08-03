0b87fd064e817d0d58908c3e6dc81af59dd6fd9b
Sarthak183 <sarthakgoyal183@gmail.com>
2019-08-03 11:00:49 +0530

    2d array rotations

---
+++ 2darrayrotate.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	int a[n][n];
+	int r;
+	
+	for(int i=0;i<n;i++)	{	
+		for(int j=0;j<n;j++)
+			scanf("%d",&a[i][j]);
+	}
+	scanf("%d",&r);
+	while(r>0)
+	{
+	int temp=0,temp1=1,l;
+	if(n>3) 
+	l=n/2;
+	else
+	l=1;
+	while(l>0)	{
+		int t=a[temp][temp];
+		int i,j;
+		j=temp;
+		for(i=temp;i<n-temp1;i++)
+			a[i][j]=a[i+1][j];
+		
+		i=n-temp1;
+		for(j=temp;j<n-temp1;j++)
+			a[i][j]=a[i][j+1];
+			
+		j=n-temp1;
+		for(i=n-temp1;i>temp;i--)
+			a[i][j]=a[i-1][j];
+		
+		
+		i=temp;
+		for(j=n-temp1;j>temp;j--)
+			a[i][j]=a[i][j-1];
+		
+		a[temp][temp1]=t;
+		temp1++;
+		temp++;
+		l--;
+	}
+	r--;}
+	for(int i=0;i<n;i++)	{
+		for(int j=0;j<n;j++)	
+			printf("%d ",a[i][j]);
+		printf("\n");
+	}
+return 0;
+}
+
