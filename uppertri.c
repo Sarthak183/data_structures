1a28884b9c946c634cf38a4e9513132d6d94d2cf
Sarthak183 <sarthakgoyal183@gmail.com>
2019-08-09 09:31:11 +0530

    upper triangular matrix

---
+++ uppertri.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void) {
+int m,n;
+scanf("%d %d",&m,&n);
+int a[m][n];
+for(int i=0 ; i<m ; i++)
+	for(int j=0 ; j<n ; j++)
+		scanf("%d",&a[i][j]);
+for(int i=0;i<m;i++) {
+	for(int j=0;j<n;j++) {
+		if(j>i)
+			printf("%d ",a[i][j]);
+		else
+			printf("  ");}
+	printf("\n");
+	}
+return 0;
+}
